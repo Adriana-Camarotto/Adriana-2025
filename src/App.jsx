0dc643b6a72bd641b21f3c242fefd89b7fdc6ea5
Adriana Camarotto <adriana.camarotto@gmail.com>
2025-06-11 18:18:23 +0100

    Fix formatting of import statement for TestimonialsCarousel in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ import { LogoCarousel } from './components/carousels/LogoCarousel'
 import { Business } from './components/business/Business'
 import { ExpertStats } from './components/ExpertStats'
 import { ContactSection } from './components/ContactSection'
-import { TestimonialsCarousel } from './components/carousels/TestimonialsCarousel'
+import {TestimonialsCarousel} from './components/carousels/TestimonialsCarousel'
+
 
 function App() {
   return (
